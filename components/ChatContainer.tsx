4283fcf829c63414d3a920cd09c085abf1cb6024
ADeMauro <demauroalice@gmail.com>
2022-05-28 21:36:12 +0200

    CMMT-01 Base structure with Tailwind
    Implement basic structure for the chat with TailwindCSS

---
+++ components/ChatContainer.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    children: React.ReactNode
+}
+
+export const ChatContainer = ({ children }: Props) => (
+    <div className="w-screen h-screen grid grid-cols-11 gap-0">
+        {children}
+    </div>
+)
+
+export default ChatContainer
